8c4e34f46e17ca7606c16245e8c5b7ee31dc6471
Tyav <tyav2greenz@gmail.com>
2020-02-06 06:20:24 +0100

    add routes to get funding requests

--- app/routes/fund.js
+++ app/routes/fund.js
@@ -9,12 +9,20 @@ const router = express.Router();
 // decode user
 router.use(decode);
 
+//load funds by id
+router.param('id', validate(validateParams.getOne, { abortEarly: false }));
+router.param('id', fundCtrl.load);
+
 router
   .route('/')
   // create user fund request
-  .post(
-    validate(validateParams.create, { abortEarly: false }),
-    fundCtrl.create
-  );
+  .post(validate(validateParams.create, { abortEarly: false }), fundCtrl.create)
+  // get all funds
+  .get(fundCtrl.getAll);
+
+router
+  .route('/:id')
+  // get funding request by id
+  .get(fundCtrl.getOne);
 
 module.exports = router;
